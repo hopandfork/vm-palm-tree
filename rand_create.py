b9f6a30a8ecbd3334efff2ae9dc7e2718009219d
Domenico Spera <speradomenico@gmail.com>
2016-11-29 18:38:11 +0100

    Base path will be used only when no other directories are specified

--- rand_create.py
+++ rand_create.py
@@ -10,41 +10,55 @@ def convert_arg_line_to_args(self, arg_line):
     return arg_line.split()
 
 
-def create_garbage():
-    max = random.randint(1000,1500)
+'''Create around size bytes of garbage'''
+def create_garbage(size):
+    approx = int(size/1000)
+    max = random.randint(size - approx,size + approx)
     chapter = ''.join(random.choice(string.printable) for _ in range(0, max))
     return chapter
 
+
+'''Create a random name for a directory'''
 def create_dname():
     max = random.randint(3, 20)
     name = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(0, max))
     return name
 
 
+'''Create a random name for a file'''
 def create_fname(ext):
     max = random.randint(3, 50)
-    name = ''.join(random.choice(string.ascii_letters + ' ' + string.digits) for _ in range(0, max)).strip() + '.' + random.choice(ext)
+    name = ''.join(random.choice(string.ascii_letters + ' ' + string.digits) for _ in range(0, max)).strip() + '.' + ext
     return name
 
 
-def create_files(n, size, exts, curr_path):
-    for file in range(0, n):
-        f_name = create_fname(exts)
-        f = open(os.path.join(curr_path, f_name), 'w')
-        curr_size = 0
-        while curr_size < size: 
-            f.write(create_garbage())
-            curr_size = f.tell()
-        f.close()
+'''Create a single file on a given path'''
+def create_file(size, exts, path):
+    f_name = create_fname(exts)
+    f = open(os.path.join(path, f_name), 'w')
+    f.write(create_garbage(size))
+    f.close()
 
 
-def create_subdirs(dirs, n, size, exts, curr_path):
+'''Create files on a set of dirs'''
+def create_files(n, size, exts, paths):
+    files_for_dir = int(n / len(paths))
+    for dir in paths:
+        for file in range(0, files_for_dir):
+            create_file(size, random.choice(exts), dir)
+
+
+'''Create sub-directories'''
+def create_subdirs(dirs, curr_path):
+    new_dirs = []
     for dir in range(0, dirs):
         d_name= create_dname()
         current_path = os.path.join(curr_path, d_name)
         Path(current_path).mkdir()
-        files_for_subdir = int(n/dirs)
-        create_files(files_for_subdir, size, exts, current_path)
+        #files_for_subdir = int(n/dirs)
+        #create_files(files_for_subdir, size, exts, current_path)
+        new_dirs.append(current_path)
+    return new_dirs
 
 
 def main(args):
@@ -54,11 +68,16 @@ def main(args):
     if Path(args.path).is_dir() is False:
         Path(args.path).mkdir()
     path = args.path
-
-    if args.directories == 0:
-        create_files(args.files, args.size, args.ext_list, path)
-    else:
-        create_subdirs(args.directories, args.files, args.size, args.ext_list, path)
+    sub_dirs = []
+    if args.existing:
+        for entry in os.listdir(path):
+            sub_dirs.append(os.path.join(path, entry))
+    if args.directories != 0:
+        sub_dirs.extend(create_subdirs(args.directories, path))
+    if len(sub_dirs) == 0:      #use root path ONLY if no other dirs are used
+        sub_dirs.append(path)
+        
+    create_files(args.files, args.size, args.ext_list, sub_dirs)
 
 
 
@@ -75,7 +94,7 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--directories', metavar='M', type=int, default=0,
                        help='number of directories to be generated (default: zero)')
 
-    parser.add_argument('-r', '--real', 
+    parser.add_argument('-r', '--real', action='store_true',
                        help='use real name for files and directories')
 
     group.add_argument('--ext-file', metavar='filename', type=str,
@@ -92,7 +111,7 @@ if __name__ == '__main__':
                         help='number of nested directories to be generated \
                         (default: no nested-dir)')
 
-    parser.add_argument('-e', '--existing', 
+    parser.add_argument('-e', '--existing', action='store_true',
                         help='use already existing sub-directories as first nested\
                         level (default: do not use already existing \
                         sub-directories)')
